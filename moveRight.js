b5007223eca666c0e161a97f1bfccb49ecfa3f8e
John M. Airey <jmairey@hotmail.com>
2015-10-28 22:44:24 -0700

    game state fixes

--- moveRight.js
+++ moveRight.js
@@ -1,23 +1,25 @@
 
 function moveCardRight(img,incr){
 
-  var imgOffsetLeft = img.offsetLeft;
-  var imgWidth    = img.offsetWidth;
-  var imgOffsetRight = imgOffsetLeft + imgWidth;
+  if (img) {
+    var imgOffsetLeft = img.offsetLeft;
+    var imgWidth    = img.offsetWidth;
+    var imgOffsetRight = imgOffsetLeft + imgWidth;
 
-  var player0CardsOffsetLeft = gPlayer0Cards.offsetLeft;
-  var player0CardsOffsetWidth = gPlayer0Cards.offsetWidth;
-  var player0CardsOffsetRight = player0CardsOffsetLeft + player0CardsOffsetWidth;
+    var player0CardsOffsetLeft = gPlayer0Cards.offsetLeft;
+    var player0CardsOffsetWidth = gPlayer0Cards.offsetWidth;
+    var player0CardsOffsetRight = player0CardsOffsetLeft + player0CardsOffsetWidth;
 
-  //console.log('img       offsetRight:',imgOffsetRight);
-  //console.log('container offsetRight:',player0CardsOffsetRight);
+    //console.log('img       offsetRight:',imgOffsetRight);
+    //console.log('container offsetRight:',player0CardsOffsetRight);
 
-  var left = parseInt(img.style.left);
+    var left = parseInt(img.style.left);
 
-  if (imgOffsetRight < player0CardsOffsetRight - incr) {
-      left += incr;
-      img.style.left = left + 'px';
-  } else {
-    /*console.log('movement right halted')*/;
+    if (imgOffsetRight < player0CardsOffsetRight - incr) {
+        left += incr;
+        img.style.left = left + 'px';
+    } else {
+      /*console.log('movement right halted')*/;
+    }
   }
 }

979e4bc618ec20837c71b822ebe755d895a0dbd9
John M. Airey <jmairey@hotmail.com>
2015-10-29 20:52:20 -0700

    introduce express and node powered server

---
+++ expressSimpleServer.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('..')); // try to serve static files under .. ?
+app.use(express.static('.')); // try to serve static files under . ?
+
+// This responds with 'Hello GET' on the home page.
+app.get('/', function(req, res) {
+  console.log("Got a GET request for the homepage");
+  res.send('Hello GET');
+});
+
+app.post('/', function(req, res) {
+  console.log("Got a POST request for the homepage");
+  res.send('Hello POST');
+});
+
+app.delete('/del_user', function(req, res) {
+  console.log("Got a DELETE request for /del_user");
+  res.send('Hello DELETE');
+});
+
+app.get('/list_user', function(req, res) {
+  console.log("got a GET request for /list_user");
+  res.send('Page Listing');
+});
+
+app.get('/gameloop',function(req, res) {
+  console.log("got a GET request for /gameloop");
+  console.log("__dirname = ",__dirname);
+  res.sendFile(__dirname + '/' + 'gameLoop2.html');
+});
+app.get('/poker',function(req, res) {
+  console.log("got a GET request for /poker");
+  console.log("__dirname = ",__dirname);
+  res.sendFile(__dirname + '/' + 'PokerHand.html');
+});
+
+app.get('/ab*cd', function(req, res) {
+  console.log("Got a GET request for /ab*cd");
+  res.send('Page Pattern Match');
+});
+
+var server = app.listen(8081, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+
+});
+
